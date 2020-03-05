90c49641a8cf7560b223e5d823eb7691aa640c03
Matavilla <mr.andrej1102@yandex.ru>
2020-03-05 16:18:49 +0300

    add new functional

--- 1/turtle_lecture.py
+++ 1/turtle_lecture.py
@@ -1,18 +1,25 @@
 from random import *
 from turtle import *
+from math import *
 
 shape("turtle")
 pensize(3)
-speed(9)
+speed(9999)
+i = 0
 while True:
     coord = pos()
-    if True or abs(coord[0]) >= window_width() / 2 or abs(coord[1]) >= window_height() / 2:
+    if abs(coord[0]) >= window_width() / 2 or abs(coord[1]) >= window_height() / 2:
 #undo()
-        penup()
-        turtlesize(randrange(1, 40))
+        turtlesize(i + randrange(1, 40),i + randrange(1, 40),i + randrange(1, 40))
         color(random(), random(), random())
-        goto(randrange(-window_width(), window_width()), randrange(-window_height(), window_height()))
-        pendown()
+        d = atan(coord[1] / coord[0]) * 180 / pi
+        right(180 * (1 + (coord[0] < 0)) - d + heading())
+        circle(randrange(10, 60), 90)
+        circle(-randrange(10, 60), 90)
+        circle(-randrange(10, 60), 90)
+        circle(randrange(10, 60), 90)
+#goto(randrange(-window_width(), window_width()), randrange(-window_height(), window_height()))
         continue
+    color(random(), random(), random())
     circle(randrange(-60,60), randrange(10, 200))
 
